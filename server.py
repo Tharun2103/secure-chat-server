80336cf790947085c9d08c6e1ff05e550cf3e79f
Tharun2103 <119412362+Tharun2103@users.noreply.github.com>
2026-03-29 23:10:14 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,18 +1,21 @@
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
+
 messages = []
 
 @app.route("/send", methods=["POST"])
 def send():
     data = request.json
+
+    # 🔥 store full message (msg + sender + id)
     messages.append(data)
+
     return {"status": "sent"}
 
 @app.route("/receive", methods=["GET"])
 def receive():
     return jsonify(messages)
 
-# 👇 IMPORTANT CHANGE
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
